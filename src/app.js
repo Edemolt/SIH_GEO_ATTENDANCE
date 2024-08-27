939f1a566c04bb568bea7cc3071b5ea69a4657c1
chaitanya-uniyal <chaitanyauniyal75@gmail.com>
2024-08-27 23:40:42 +0530

    initialized and structured the repo

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import express, { json } from 'express';
+import userRouter from '../routes/userRoutes.js'; // Import the userRouter
+
+const app = express();
+
+app.use(json());
+app.use('/users', userRouter); // Use the userRouter for all /users routes
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
