f21d555989ba86c27af05b347d04935933e56c2f
Vaishnavi Mittal <vaishnavimittal86@gmail.com>
2024-09-08 21:03:16 +0530

    mappage

--- admin_app/lib/login.dart
+++ admin_app/lib/login.dart
@@ -1,4 +1,73 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(),
+      darkTheme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
+      initialRoute: 'login', // Start with the login page
+      routes: {
+        'login': (context) => const MyLogin(), // Login page route
+        'map': (context) => const MapPage(), // Map page route
+      },
+    );
+  }
+}
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _mapController =
+      Completer<GoogleMapController>();
+
+  static const LatLng _location1 = LatLng(30.316496, 78.032188);
+  static const LatLng _location2 = LatLng(29.380978, 79.528519);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Map Page'),
+      ),
+      body: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          _mapController.complete(controller);
+        },
+        initialCameraPosition: const CameraPosition(
+          target: _location1,
+          zoom: 13,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId("location1"),
+            position: _location1,
+            infoWindow: const InfoWindow(title: "Dehradun"),
+          ),
+          Marker(
+            markerId: const MarkerId("location2"),
+            position: _location2,
+            infoWindow: const InfoWindow(title: "Graphic Era University"),
+          ),
+        },
+      ),
+    );
+  }
+}
 
 class MyLogin extends StatefulWidget {
   const MyLogin({Key? key}) : super(key: key);
@@ -14,20 +83,10 @@ class _MyLoginState extends State<MyLogin> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //appBar: AppBar(
-      // title: const Text(
-      //   "MAJDOOR",
-      //   style: TextStyle(
-      //     fontFamily: 'YatraOne',
-      //     fontSize: 48,
-      //    ),
-      //  ),
-      //),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(20, 10, 20, 50),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          //crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             const Center(
                 child: Text(
@@ -102,7 +161,9 @@ class _MyLoginState extends State<MyLogin> {
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.purple,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    // Add registration logic here
+                  },
                   child: const Text('Register'),
                 ),
               ],
--- admin_app/lib/main.dart
+++ admin_app/lib/main.dart
@@ -1,18 +1,63 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:admin_app/login.dart';
-import 'package:admin_app/Pages/map_page.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       theme: ThemeData(),
       darkTheme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      initialRoute: 'login',
-      routes: {
-        'login': (context) => const MyLogin(),
-        'map': (context) => const MapPage(),
-      },
-    ),
-  );
+      home: const MapPage(),
+    );
+  }
+}
+
+class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
+  @override
+  State<MapPage> createState() => _MapPageState();
+}
+
+class _MapPageState extends State<MapPage> {
+  final Completer<GoogleMapController> _mapController =
+      Completer<GoogleMapController>();
+
+  static const LatLng _location1 = LatLng(30.316496, 78.032188);
+  static const LatLng _location2 = LatLng(30.3283021, 78.0586985);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {
+          _mapController.complete(controller);
+        },
+        initialCameraPosition: const CameraPosition(
+          target: _location1,
+          zoom: 13,
+        ),
+        markers: {
+          Marker(
+            markerId: const MarkerId("location1"),
+            position: _location1,
+            infoWindow: const InfoWindow(title: "Location 1"),
+          ),
+          Marker(
+            markerId: const MarkerId("location2"),
+            position: _location2,
+            infoWindow: const InfoWindow(title: "Location 2"),
+          ),
+        },
+      ),
+    );
+  }
 }
